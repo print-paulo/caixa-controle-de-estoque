3fd756f58108237c8e4798ef3322ab2ca791604d
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2026-07-10 23:20:34 -0300

    Refactor product registration process: streamline input handling and remove redundant code

--- tests/cadastro_teste.py
+++ tests/cadastro_teste.py
@@ -2,62 +2,8 @@ import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from services.excluir_produto import excluir_produto_permanente
+from services.cadastrar_produto import executar_cadastro
 
-from services.cadastrar_produto import (
-    cadastrar_produto_base,
-    adicionar_categoria,
-    adicionar_codigo_barras_com_leitor,
-    adicionar_quantidade,
-    adicionar_medida_quantidade,
-    adicionar_unidade,
-    adicionar_valor_unitario,
-    adicionar_estoque_deposito,
-    adicionar_estoque_exposicao,
-    adicionar_codigo_barras_interativo
-)
-
-def executar_cadastro(codigo_barras=None):
-        while True:
-
-            try:
-                nome = input("Nome do produto: ").upper()
-                categoria = input("Categoria: ").upper()
-
-                # Cria o produto e obtém o ID
-                id_produto = cadastrar_produto_base(nome)
-
-                adicionar_categoria(id_produto, categoria)
-
-                # Se aqui der erro, cai no except, e funciona a repetir caso use "sair"
-                if not adicionar_codigo_barras_interativo(id_produto, codigo_barras):
-                    excluir_produto_permanente(id_produto)
-                    print("Cadastro Cancelado")
-                    continue
-
-                quantidade = int(input("Quantidade: "))
-                while (medida := input("Medida da quantidade: ").upper()) not in ["MG", "ML", "G", "L", "KG"]:
-                    print("Medida inválida. Por favor, escolha uma das medidas válidas. (MG/ML/G/L/KG)\n")
-
-                while (unidade := input("Unidade: ").upper()) not in ["UN", "CX"]:
-                    print("Unidade inválida. Por favor, escolha uma das unidades válidas. (UN/CX)\n")
-                adicionar_quantidade(id_produto, quantidade)
-                print(medida)
-                adicionar_medida_quantidade(id_produto, medida)
-                print(unidade)
-                adicionar_unidade(id_produto, unidade)
-
-                print(f"\nProduto cadastrado com sucesso! ID: {id_produto}")
-                break
-
-            except ValueError as e:
-
-                print(f"\nErro: {e}")
-
-                # Remove o cadastro incompleto
-                if "id_produto" in locals():
-                    excluir_produto_permanente(id_produto)
-                    del id_produto
 
 if __name__ == "__main__":
     executar_cadastro()
