ea05c2fdc54a5ecc67228116646f74709d3f8ae0
Mateus Neves <mateusgameroficialbr@gmail.com>
2026-07-16 00:48:16 -0300

    Corrigido import

--- tests/editar_teste.py
+++ tests/editar_teste.py
@@ -2,7 +2,8 @@ import sys
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from services.editar_produto import executar_edicao
+
+from controllers.produto_controller import executar_edicao
 
 if __name__ == "__main__":
     executar_edicao()
